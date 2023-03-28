63ad9c3a6997d6f98c2d331d0dfbc8c5e199b8a1
Alice Chen <zixic@student.unimelb.edu.au>
2023-03-28 16:31:16 +1100

    fetch existing tweets at first render

--- my-twitter/src/Feed.js
+++ my-twitter/src/Feed.js
@@ -1,12 +1,34 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./Feed.css";
 import TweetBox from "./TweetBox";
 import Post from "./Post";
 
 function Feed() {
   const [fetchedData, setFetchedData] = useState("");
+  useEffect( () => {
+    getAllTweets();
+  }, []);
 
-
+  const getAllTweets = async () => {
+    const allTweets = await axios.get("http://localhost:5000/allTweets");
+    setFetchedData(allTweets.data)
+    if (Object.keys(fetchedData).length !== 0) {
+      fetchedData
+        .slice(0)
+        .reverse()
+        .map((post) => {
+          return (
+            <Post
+              displayName="大器晩成"
+              text={post.tweetText}
+              image={post.tweetMedia}
+              avatar="https://images.unsplash.com/photo-1563306206-900cc99112fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80"
+            />
+          );
+        });
+    }
+  };
 
   return (
     <div className="feed">
@@ -18,16 +40,19 @@ function Feed() {
       <TweetBox setFetchedData={setFetchedData} />
       {/* go through each tweet in the allTweets json */}
       {Object.keys(fetchedData).length !== 0
-        ? fetchedData.slice(0).reverse().map((post) => {
-            return (
-              <Post
-                displayName="大器晩成"
-                text={post.tweetText}
-                image={post.tweetMedia}
-                avatar="https://images.unsplash.com/photo-1563306206-900cc99112fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80"
-              />
-            );
-          })
+        ? fetchedData
+            .slice(0)
+            .reverse()
+            .map((post) => {
+              return (
+                <Post
+                  displayName="大器晩成"
+                  text={post.tweetText}
+                  image={post.tweetMedia}
+                  avatar="https://images.unsplash.com/photo-1563306206-900cc99112fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80"
+                />
+              );
+            })
         : null}
     </div>
   );
