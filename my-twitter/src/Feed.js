db6bd66d2bad0df5cbead62dc88683e8ed98c1a2
Alice Chen <zixic@student.unimelb.edu.au>
2023-03-28 15:10:02 +1100

    able to fetch data from DB

--- my-twitter/src/Feed.js
+++ my-twitter/src/Feed.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Feed.css";
 import TweetBox from "./TweetBox";
 import Post from "./Post";
@@ -6,21 +6,7 @@ import Post from "./Post";
 function Feed() {
   const [fetchedData, setFetchedData] = useState("");
 
-  function fetchData() {
-    if (Object.keys(fetchedData).length !== 0) {
-      fetchedData.map((post) => {
-        console.log(post.tweetText)
-        return (
-          <Post
-            displayName="大器晩成"
-            text={post.tweetText}
-            image={post.tweetMedia}
-            avatar="https://images.unsplash.com/photo-1563306206-900cc99112fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80"
-          />
-        );
-      });
-    }
-  }
+
 
   return (
     <div className="feed">
@@ -31,18 +17,18 @@ function Feed() {
       {/*Tweet Box */}
       <TweetBox setFetchedData={setFetchedData} />
       {/* go through each tweet in the allTweets json */}
-      {fetchData()}
-      {/* <Post
-          displayName="大器晩成"
-          text={fetchedData.tweetText}
-          image={fetchedData.tweetMedia}
-          avatar="https://images.unsplash.com/photo-1563306206-900cc99112fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80"
-        /> */}
-      {/*Post */}
-      {/*Post */}
-      {/*Post */}
-      {/*Post */}
-      {/*Post */}
+      {Object.keys(fetchedData).length !== 0
+        ? fetchedData.slice(0).reverse().map((post) => {
+            return (
+              <Post
+                displayName="大器晩成"
+                text={post.tweetText}
+                image={post.tweetMedia}
+                avatar="https://images.unsplash.com/photo-1563306206-900cc99112fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80"
+              />
+            );
+          })
+        : null}
     </div>
   );
 }
