2080a9e4b8bf3d6f75ba632d8552dd7f67fb92b2
Zixi Chen <alicecnana@hotmail.com>
2023-04-04 22:21:40 +1000

    update date format

--- my-twitter/src/Post.js
+++ my-twitter/src/Post.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import TimeAgo from "react-timeago";
+import Moment from "react-moment";
 import ReactPlayer from "react-player";
 import UpdateBox from "./UpdateBox";
 import "./Post.css";
@@ -25,11 +26,17 @@ function Post({ displayName, text, image, video, timestamp, avatar, id, deletePo
         <div>
           <div className="flex items-center space-x-1">
             <h3 className="mr-1 font-bold">{displayName}</h3>
-            <TimeAgo date={timestamp} className="text-sm text-gray-500" />
+            <Moment format="MMM DD HH:mm" className="text-sm text-gray-500">
+              {timestamp}
+            </Moment>
           </div>
           <p className="pt-1">{text}</p>
-          {image ? <img className="imgFig p-1 pt-3" src={image} alt="" /> : null}
-          {video ? <ReactPlayer className='pt-3' url={video} controls={true} /> : null}
+          {image ? (
+            <img className="imgFig p-1 pt-3" src={image} alt="" />
+          ) : null}
+          {video ? (
+            <ReactPlayer className="pt-3" url={video} controls={true} />
+          ) : null}
         </div>
       </div>
 
@@ -38,8 +45,19 @@ function Post({ displayName, text, image, video, timestamp, avatar, id, deletePo
           <ChatBubbleBottomCenterIcon className="h-5 w-5 flex cursor-pointer items-center space-x-3 text-gray-400" />
         </div>
         <div>
-          <PencilSquareIcon className="h-5 w-5 flex cursor-pointer items-center space-x-3 text-gray-400" onClick={() => setShowBox(true)} />
-          {showBox ? <UpdateBox id={id} updatePost={updatePost} setShowBox={setShowBox} showBox={showBox} text={text} />:null}
+          <PencilSquareIcon
+            className="h-5 w-5 flex cursor-pointer items-center space-x-3 text-gray-400"
+            onClick={() => setShowBox(true)}
+          />
+          {showBox ? (
+            <UpdateBox
+              id={id}
+              updatePost={updatePost}
+              setShowBox={setShowBox}
+              showBox={showBox}
+              text={text}
+            />
+          ) : null}
         </div>
         <div>
           <TrashIcon
